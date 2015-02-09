f154ef85503893205dbe8e584819a2dd7ac2df4b
Silvia Oliveros <soliverost@gmail.com>
2015-02-09 00:50:54 -0800

    Streaming now saving to a single file in HDFS - Yay to avoiding small files

--- RealTime/PlayStreaming/src/main/scala/playstreaming.scala
+++ RealTime/PlayStreaming/src/main/scala/playstreaming.scala
@@ -10,6 +10,11 @@ import org.apache.spark.sql.cassandra.CassandraSQLContext
 import com.datastax.spark.connector._
 import com.datastax.spark.connector.streaming._
 
+import java.io._
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
+
 
 object playstreaming {
 
@@ -37,13 +42,19 @@ object playstreaming {
                     .set("spark.cassandra.connection.host", "54.67.124.220")
     val sc = new SparkContext(conf)
 
-    val ssc =  new StreamingContext(sc, Seconds(7))
-    ssc.checkpoint("checkpoint")
+    val hadoopconf = new Configuration()
+    val fs= FileSystem.get(hadoopconf)
+    val path = new Path("/user/solivero/fantasyfootball/userpoints_test/dom2")
+    val output = if(fs.exists(path)) fs.append(path) else fs.create(path)
+    val writer = new PrintWriter(output)
 
     // 1, 1, Bruce Gradkowski, QB, 2014-08-02 10:00:00 -0700
     val file = sc.textFile("hdfs://ip-172-31-13-111.us-west-1.compute.internal:8020/user/solivero/fantasyfootball/playerselection/newOutput.csv")
     val playeruser = file.map({line => val pieces = line.split(",")
         (pieces(2),pieces(1).toInt)})
+
+    val ssc =  new StreamingContext(sc, Seconds(10))
+    ssc.checkpoint("checkpoint")
 
     // Kafka consumer
     val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topics)
@@ -51,12 +62,24 @@ object playstreaming {
     // Map my kafka stream to the correct key,value:
     val testInput = lines.map({line => val pieces = line._2.split(",")
         (pieces(2),(pieces(3),pieces(0)))})
-    // testInput.print()
    
     // Join with the appropiate user and flatten
     val joinedUserPoints = testInput.transform(rdd=> rdd.join(playeruser))
     val userPointsFlat = joinedUserPoints.map{case(pname,((points,date),uid)) => (uid,date,points)}
-    // TODO: Save to HDFS
+    // userPointsFlat.repartition(1).saveAsTextFiles("/user/solivero/fantasyfootball/userpoints_test/dom","csv")
+
+    // Loop through and save in a single file to avoid lots of small files
+    userPointsFlat.foreachRDD(rdd => {
+        for(item <- rdd.collect().toArray) {
+            try {
+                writer.append("" + item._1)
+                writer.append("," + item._2)
+                writer.append("," + item._3)
+                writer.append('\n') 
+            }
+        }
+    })
+
     
     // Keep track of the score of the day
     val userPointsDay = userPointsFlat.map{case(uid,date,points) => ((uid,date),points.toDouble)}
@@ -65,19 +88,13 @@ object playstreaming {
     runningCountsFlat.saveToCassandra("fantasyfootball","userpointsstream",SomeColumns("userid","date","points"))
 
 
-
-
-    // New RDD from Cassandra Query
-    // val playeruser = ssc.cassandraTable("playerfollowers", "playeruser").select("playername","userid")
-
-    /*
-    val stream_window_element = lines.countByWindow(Seconds(5), Seconds(1))
-    stream_window_element.print()*/
-
-
-
     ssc.start()
     ssc.awaitTermination()
+
+
+    // close the writer
+    writer.close()
+
   }
 }
 
